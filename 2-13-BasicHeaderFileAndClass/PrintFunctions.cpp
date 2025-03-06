2c6a1b14faba15e0a6ea20fbe77e834a50b7dab5
PersnicketyChkn <chrisajohnson234@gmail.com>
2025-03-06 09:20:26 -0800

    Add files via upload

---
+++ 2-13-BasicHeaderFileAndClass/PrintFunctions.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using std::cout;
+//#include "PrintFunctions.h"
+/*
+Prints an integer that is passed by value
+*/
+void PrintNum(int num)
+{
+	cout << num;
+}
+
+void PrintFloat(float num)
+{
+	cout << num;
+}
+
+void PrintChar(char c)
+{
+	cout << c;
+}
+
+void PrintDouble(double num)
+{
+	cout << num;
+}
